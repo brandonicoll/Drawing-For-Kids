f89b9091ef43a35667cce329493ee2f233734a78
Brandon <brandonspersonalmailbox@gmail.com>
2021-06-28 14:55:07 -0400

    Setup public function to set brush size -> to be called in main oncreate().

--- app/src/main/java/com/example/drawingforkids/DrawingView.kt
+++ app/src/main/java/com/example/drawingforkids/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.drawingforkids
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -81,6 +82,11 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG) //blitting in graphics
         mBrushSize = 20.toFloat()
+    }
+
+    fun setSizeForBrush(newSize: Float) {
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
 
